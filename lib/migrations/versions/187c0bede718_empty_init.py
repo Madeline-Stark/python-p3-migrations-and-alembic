fa255412cc68a4d3b1e72d1883447751d5aaab9a
Madeline Stark <17558341+Madeline-Stark@users.noreply.github.com>
2023-01-08 11:15:19 -0800

    pass

---
+++ lib/migrations/versions/187c0bede718_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 187c0bede718
+Revises: 
+Create Date: 2023-01-08 10:56:01.408504
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '187c0bede718'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
